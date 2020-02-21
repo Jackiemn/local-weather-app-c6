fe1929a044a89efa5d61c6671f2f042064632ce5
Jackiemn <59993378+Jackiemn@users.noreply.github.com>
2020-02-20 22:29:18 -0800

    change package json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,26 @@ import { AppComponent } from './app.component';
 import { CurrentWeatherComponent } from './current-weather/current-weather.component';
 import {WeatherService} from './weather.service'
 import{HttpClientModule} from '@angular/common/http';
+import { CitySearchComponent } from './city-search/city-search.component';
+import 'hammerjs';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CurrentWeatherComponent
+    CurrentWeatherComponent,
+    CitySearchComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
   ],
   providers: [WeatherService],
   bootstrap: [AppComponent]
