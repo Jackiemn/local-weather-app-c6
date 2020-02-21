54efd5fea84bd4525c54c6a9b1a5bb3b2061a2ee
Jackiemn <59993378+Jackiemn@users.noreply.github.com>
2020-02-21 01:04:27 -0800

    changes city search

--- src/app/city-search/city-search.component.ts
+++ src/app/city-search/city-search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, Validators} from '@angular/forms';
 import { debounceTime } from 'rxjs/operators';
+import { WeatherService } from '../weather.service';
 
 @Component({
   selector: 'app-city-search',
@@ -14,13 +15,13 @@ search = new FormControl('', [Validators.minLength(3)]);
   ngOnInit() {
     this.search.valueChanges
     .pipe(debounceTime(1000))
-    .Subscribe(
+    .subscribe(
       (searchValue: string) => {
-if (!this,search.invalid && searchValue){
+if (!this.search.invalid && searchValue){
 const userInput = searchValue.split(',').map(s=>s.trim());
 this.weatherService.getCurrentWeather(
   userInput[0],
-  userInput.length > 1 ? userInput[1]: undefined).Subscribe(data => console.log(data));
+  userInput.length > 1 ? userInput[1]: undefined).subscribe(data => console.log(data));
 }
       })
   }
